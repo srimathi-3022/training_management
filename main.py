b0f37f79322e0f698e07f34ddb56dc671ab207c9
srimathi-3022 <srimathi.s@codincity.com>
2026-05-21 10:46:10 +0530

    After writing the integration code with front end

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import course_routes, trainee_routes, registration_routes, attendance_routes
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(course_routes.router)
 app.include_router(trainee_routes.router)
